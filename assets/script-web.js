960abd672137610ce0345890fb811cb37d16cecc
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-04-23 22:55:23 +0300

    Update script-web.js

--- assets/script-web.js
+++ assets/script-web.js
@@ -1,25 +1,28 @@
-$(document).ready(function() {
+window.onload = function() {
 
-  const runButton = $("#run-code-button");
-  const newPagePreview = $("#new-page-preview-button");
-  const resetButton = $("#reset-editors-button");
-  const form = $("form");
+  const runButton = document.getElementById("run-code-button");
+  const newPagePreview = document.getElementById("new-page-preview-button");
+  const resetButton = document.getElementById("reset-editors-button");
+  const form = document.querySelector("form");
+  const list = document.getElementsByClassName("textarea-code");
 
 // Display default code on window load.
-  runButton.click(function () {
-    form.attr("target", "iframe-result");
+  runButton.onclick = function () {
+    form.setAttribute("target", "iframe-result");
     form.submit();
-  });
+  };
 
 // Display result on a different page.
-  newPagePreview.click(function () {
-    form.attr("target", "_blank");
+  newPagePreview.onclick = function () {
+    form.setAttribute("target", "_blank");
     form.submit();
-  });
+  };
 
 // Empty all editors' code space.
-  resetButton.click(function () {
-    $(".textarea-code").html("");
-});
+  resetButton.onclick = function () {
+    for (i = 0; i < list.length; i++) {
+      list[i].innerHTML = "";
+    }
+};
 
-});
+};
