7e8c66ea3ca3d9f425b2ef77d56fa1df9d921639
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-04-23 20:05:51 +0300

    Update script-sign-up.js

--- assets/script-sign-up.js
+++ assets/script-sign-up.js
@@ -1,11 +1,1 @@
-window.onload = function(){
 
-  const signUpFormPreview = document.getElementById("signupform");
-  const homePage = document.getElementById("homepage");
-  const signUpPage = document.getElementById("signuppage");
-  if (signUpFormPreview) {
-    homePage.toggleClass("active");
-    signUpPage.classList.add("active");
-  }
-
-}
