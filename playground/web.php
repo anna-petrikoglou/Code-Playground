bf8cdba8d8b35b902066a3cf354623771cfbe5c6
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-04-29 13:08:36 +0300

    Add jQuery library
    Also, put <script> tag at the end of the <body> tag.

--- playground/web.php
+++ playground/web.php
@@ -8,14 +8,15 @@ if(!isset($_POST['html']) or !isset($_POST['css']) or !isset($_POST['javascript'
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <style type="text/css">
       <?php echo $_POST['css']; ?>
     </style>
-    <script>
-      <?php echo $_POST['javascript']; ?>
-    </script>
   </head>
   <body>
     <?php echo $_POST['html']; ?>
+    <script>
+      <?php echo $_POST['javascript']; ?>
+    </script>
   </body>
 </html>
