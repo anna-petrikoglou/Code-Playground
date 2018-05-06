1a6579c4cb995e25b29bb3c89611e3c68d35bb9c
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-06 19:26:43 +0300

    Create database

---
+++ system/database.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost:3308";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "code_playground";
+
+$connection = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
