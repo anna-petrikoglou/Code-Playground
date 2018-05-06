d31cde6cb3ea2c88123e8ec6d8bc60b315da63e5
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-06 19:23:18 +0300

    Differentiate connected user's menu

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -12,12 +16,31 @@
       <ul class="nav navbar-nav">
         <li><a href="index.php">Home</a></li>
         <li><a href="web.php">Web</a></li>
-        <li class="disabled"><a href="#">PHP</a></li>
-        <li class="disabled"><a href="#">MySQL</a></li>
+        <?php
+          if (isset($_SESSION['u_id'])) {
+            echo '<li><a href="#">PHP</a></li>
+                  <li><a href="#">MySQL</a></li>';
+          } else {
+            echo '<li class="disabled"><a href="#">PHP</a></li>
+                  <li class="disabled"><a href="#">MySQL</a></li>';
+          }
+        ?>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li id="signuppage"><a href="signup.php">Sign up</a></li>
-        <li><a href="login.php">Login</a></li>
+        <?php
+          if (isset($_SESSION['u_id'])) {
+            //Use this same isset() function to change any content on any page, when a user is logged in.
+            //echo "You are logged in!";
+            // <input id="logoutInput" type="hidden" name="logoutInput" value="N/A"/> value attribute?
+            echo '<form class="nav navbar-nav navbar-right" action="system/logout.php" method="post" id="logoutForm">
+                  <button class="btn" id="logoutButton" type="submit" name="submit">Logout</button>
+                  </form>
+                  <li><a href="#">Logout</a></li>';
+          } else {
+            echo '<li><a href="signup.php">Sign up</a></li>
+                  <li><a href="login.php">Login</a></li>';
+          }
+          ?>
         <li><a href="#">About</a></li>
       </ul>
     </div>
