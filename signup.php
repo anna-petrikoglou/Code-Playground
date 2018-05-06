eda56313f5cfb618840fabe7e6d8e0b975f647d7
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-06 19:04:42 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,29 +7,35 @@
       <h4>Gain access to more features, additional functionalities and powerful tools!</h4>
     </div>
     <div class="container">
-      <form class="form-horizontal" action="/action_page.php">
+      <form class="form-horizontal" action="system/signup.php" method="post">
         <div class="form-group">
-          <label class="control-label col-sm-3" for="name">Name:</label>
+          <label class="control-label col-sm-3" for="first-name">First Name:</label>
           <div class="col-sm-6">
-            <input type="text" autocomplete="user-name" class="form-control" id="name" placeholder="Enter your name" name="name" required maxlength="60">
+            <input type="text" autocomplete="off" class="form-control" id="first-name" placeholder="Enter your first name" name="first-name" required maxlength="50">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-3" for="last-name">Last Name:</label>
+          <div class="col-sm-6">
+            <input type="text" autocomplete="off" class="form-control" id="last-name" placeholder="Enter your last name" name="last-name" required maxlength="70">
           </div>
         </div>
         <div class="form-group">
           <label class="control-label col-sm-3" for="email">Email:</label>
           <div class="col-sm-6">
-            <input type="email" autocomplete="user-email" class="form-control" id="email" placeholder="Enter your email" name="email" required maxlength="60">
+            <input type="text" autocomplete="off" class="form-control" id="email" placeholder="Enter your email" name="email" required maxlength="100">
           </div>
         </div>
         <div class="form-group">
-          <label class="control-label col-sm-3" for="pwd">Password:</label>
+          <label class="control-label col-sm-3" for="username">Username:</label>
           <div class="col-sm-6">
-            <input type="password" autocomplete="user-password" class="form-control" id="pwd" placeholder="Type your password" name="pwd" required maxlength="60">
+            <input type="text" autocomplete="off" class="form-control" id="username" placeholder="Type your username" name="username" required maxlength="100">
           </div>
         </div>
         <div class="form-group">
-          <label class="control-label col-sm-3" for="re-pwd">Password:</label>
+          <label class="control-label col-sm-3" for="password">Password:</label>
           <div class="col-sm-6">
-            <input type="password" autocomplete="user-password-repeat" class="form-control" id="re-pwd" placeholder="Retype your password" name="pwd" required maxlength="60">
+            <input type="password" autocomplete="off" class="form-control" id="password" placeholder="Type your password" name="password" required maxlength="256">
           </div>
         </div>
         <div class="form-group">
@@ -42,7 +48,7 @@
         </div>
         <div class="form-group">
           <div class="col-sm-offset-3 col-sm-9">
-            <button type="submit" class="btn">Submit</button>
+            <button type="submit" class="btn" name="submit">Submit</button>
             <a href="index.php" class="btn btn-default cancel">Cancel</a>
           </div>
         </div>
@@ -52,5 +58,4 @@
   <!-- Image
   <div id="signupimagedown" class="container">
   </div>-->
-  <script src="assets/script-sign-up.js" type="text/javascript"></script>
   <?php require 'includes/footer.php'; ?>
