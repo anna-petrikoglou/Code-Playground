bd769e870c894b82d66370afb186f6f3d1c37e72
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-13 22:58:59 +0300

    Update web.js

--- assets/web.js
+++ assets/web.js
@@ -1,10 +1,11 @@
-window.onload = function() {
+window.onload = function () {
 
   const runButton = document.getElementById("run-code-button");
   const newPagePreview = document.getElementById("new-page-preview-button");
   const resetButton = document.getElementById("reset-editors-button");
   const form = document.getElementById("webform");
   const list = document.getElementsByClassName("textarea-code");
+  const saveButton = $("#save-project-button");
 
 // Display default code on window load.
   runButton.onclick = function () {
@@ -23,6 +24,25 @@ window.onload = function() {
     for (i = 0; i < list.length; i++) {
       list[i].innerHTML = "";
     }
-};
+  };
+
+  saveButton.click(function () {
+    // Assign handlers immediately after making the request,
+    // and remember the jqxhr object for this request
+    //{ html: $("#htmleditor textarea").val(), css: $("#csseditor textarea").val(), javascript: $("#jseditor textarea").val() }
+    let jqxhr = $.post("system/save.php", $("#webform").serialize());
+
+    jqxhr.done(function(data) {
+      alert("Project saved!");
+      if ($('#project-id').val() === "") {
+      window.location.href = '/web.php$project_id=' + data;
+    }
+    })
+    
+    jqxhr.fail(function() {
+      alert("error");
+    })
+
+  });
 
 };
