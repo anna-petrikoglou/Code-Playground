238d8d63e992efec93bed2f698fe9107ae87a1f7
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-13 17:46:26 +0300

    Add files via upload

---
+++ playground/css.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style type="text/css">
+      <?php echo $_POST['css']; ?>
+    </style>
+  </head>
+  <body>
+    <?php echo $_POST['html']; ?>
+  </body>
+</html>
