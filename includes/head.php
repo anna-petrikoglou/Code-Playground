8ee65fae85347bd9754ab732d9cb069b1b27b9f5
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-13 17:43:51 +0300

    favicon missing

--- includes/head.php
+++ includes/head.php
@@ -10,8 +10,8 @@
     <!--<meta http-equiv="X-UA-Compatible" content="IE=edge"> ???-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-    <!--<link rel="shortcut icon" type="image/x-icon href="favicon.ico"">-->
-    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+    <!--<link rel="shortcut icon" type="image/x-icon href="favicon.ico"">
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
