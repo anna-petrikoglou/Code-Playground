fef4cad0eb261df3ebd53047e66bff3f3074b015
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-05-13 17:45:36 +0300

    create content

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,13 @@
     <!--<time datetime="2018-04-20T12:15:50+00:00">April 20, 2018</time>-->
       <div class="col-md-12 intro">
         <h1>Hey there!</h1>
-        <h4>Got a great idea? About to create something new? Just practicing?<br>Join us! Learn more <a href="#" class="basic-color">here</a>.</h4>
+        <?php
+          if (isset($_SESSION['u_id'])) {
+            echo '<h4>Welcome back! How are your projects going? Anything new to add?</h4>';
+          } else {
+            echo '<h4>Got a great idea? About to create something new? Just practicing?<br>Join us! Learn more <a href="#" class="basic-color">here</a>.</h4>';
+          }
+        ?>
       </div>
   </div>
 </header>
