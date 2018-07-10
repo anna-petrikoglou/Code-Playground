12808a85642cb761aea5f4e5c84fdccf33f02679
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-07-10 13:53:51 +0300

    Set session's lifetime

--- system/settings.php
+++ system/settings.php
@@ -5,6 +5,29 @@
 
   session_start();
 
+  // Set session lifetime period (in seconds)
+  $session_lifetime = 1800;
+
+  // Check to see if $_SESSION["time"] is set
+  if (isset($_SESSION["time"])) {
+
+    // Calculate the session's lifetime
+    $duration = time() - $_SESSION["time"];
+
+    if ($duration > $session_lifetime) {
+      session_unset();
+      session_destroy();
+      // Page redirection
+      header("Location: ../login.php");
+    }
+
+  } else {
+
+    // Keep session alive
+    $_SESSION["time"] = time();
+
+  }
+
   register_shutdown_function(function() {
     unset($_SESSION['submission']);
   });
