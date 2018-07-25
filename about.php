b6d7bad339ed5667ab3c2363203df85b6db848bb
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-07-25 14:46:40 +0300

    Make content improvements

--- about.php
+++ about.php
@@ -12,8 +12,14 @@ require 'includes/nav.php';
   function scrollFunction() {
     if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
       document.getElementById("scroll-to-top").style.display = "block";
+      document.getElementById("go-to-blog").style.display = "block";
+      document.getElementById("go-to-tools").style.display = "block";
+      document.getElementById("go-to-help").style.display = "block";
     } else {
       document.getElementById("scroll-to-top").style.display = "none";
+      document.getElementById("go-to-blog").style.display = "none";
+      document.getElementById("go-to-tools").style.display = "none";
+      document.getElementById("go-to-help").style.display = "none";
     }
   }
 </script>
@@ -44,7 +50,7 @@ require 'includes/nav.php';
         ?>
       </div>
       <div class="col-md-4 about rotate">
-        <img src="img/magnifier.png" alt="A magnifying glass." id="magnifier">
+        <img src="img/magnifier.png" alt="Magnifying glass" id="magnifier">
       </div>
       <div class="col-md-2 about-control-panel">
         <ul class="nav">
@@ -55,8 +61,11 @@ require 'includes/nav.php';
         </ul>
       </div>
     </div>
-    <div class="move-to-top" id="scroll-to-top" style="display: none"><a href="#home-menu"><img src="img/arrow.png" alt="Top arrow"></a></div>
   </div>
+  <div class="move-to" id="go-to-tools" style="display: none;"><a href="account.php"><img src="img/tool.png" alt="Tool icon" title="Settings"></a></div>
+  <div class="move-to" id="go-to-blog" style="display: none;"><a href="blog.php"><img src="img/chat.png" alt="Chat icon" title="Blog"></a></div>
+  <div class="move-to" id="go-to-help" style="display: none;"><a href="help.php"><img src="img/help.png" alt="Question mark" title="Help"></a></div>
+  <div class="move-to" id="scroll-to-top" style="display: none;"><a href="#home-menu"><img src="img/arrow.png" alt="Top arrow" title="Top"></a></div>
   <div class="presentation about-services" id="services">
     <div class="container">
       <div class="row">
@@ -89,7 +98,7 @@ require 'includes/nav.php';
       </div>
       </div>
       <div class="col-md-6 about">
-        <img src="img/slide.png" alt="A chart with connected dots.">
+        <img src="img/puzzle.png" alt="Puzzle pieces" >
       </div>
     </div>
   </div>
@@ -101,7 +110,7 @@ require 'includes/nav.php';
         </div>
       </div>
       <div class="col-md-6 story">
-        <p>We aim to provide efficient services and consistent access to the up-to-date material. For these reasons, we maintain, debug and renew the website on a regular basis. However, there may still be lots of malfunctions. We highly recommend that you use the Google Chrome or the Mozilla Firefox browser.</p><p>Security scores do not denote vulnerabilities, regarding sensitive data. We would like to assure you that any personal information we store is highly protected and safe. To report issues while running the app, please complete the contact form provided below or click <a href="https://docs.google.com/forms/d/e/1FAIpQLScKWPsrGPX-fWZ3qgZBKEKuW0FMkb4vWP9WZcjwZufBwmdx1Q/viewform?usp=sf_link" target="_blank" class="basic-color">here</a>.</p>
+        <p>We aim to provide efficient services and consistent access to the up-to-date material. For these reasons, we maintain, debug and renew the website on a regular basis. However, there may still be lots of malfunctions. We highly recommend that you use the Google Chrome, the Opera or the Mozilla Firefox browser, which have already been tested.</p><p>Security scores do not denote vulnerabilities, regarding sensitive data. We would like to assure you that any personal information we store is highly protected and safe. To report issues while running the app, please complete the contact form provided below or click <a href="https://docs.google.com/forms/d/e/1FAIpQLScKWPsrGPX-fWZ3qgZBKEKuW0FMkb4vWP9WZcjwZufBwmdx1Q/viewform?usp=sf_link" target="_blank" class="basic-color">here</a>.</p>
         <p><span class="basic-color">Secret! </span>As a matter of fact, we have never taken these tests. (Oops!) The following scores simply represent our perception and estimations on the satisfaction level of each aspect. But, trust me. We are not that indulgent!</p>
         <!--<img src="img/analytics.png" alt="Bars of analysis and arrows.">-->
       </div>
@@ -125,11 +134,11 @@ require 'includes/nav.php';
         </div>
       </div>
       <div class="col-md-6 about">
-        <img src="img/web-dev.png" alt="Fixing a code editor.">
+        <img src="img/study.png" alt="A pencil, a clock, a lamp, a screen, a document, a magnifier and a loudspeaker" style="opacity: 1;">
       </div>
       <div class="col-md-6 story">
         <p>Here we go! You may have come across plenty of code playgrounds. Our goal is to offer you a comprehensive understanding of how fundamental web technologies operate, interact and shape entire applications and websites. Ok, you have to do most of it by yourself. But, you have valuable development tools!</p>
-        <p>Learn how to create static and dynamic web pages, add functionalities, enhance content, make responsive designs and much more. Savvy client-server communication and database management.</p>
+        <p>Learn how to create static and dynamic web pages, add functionalities, enhance content, make responsive designs and much more. Savvy client - server communication and database management.</p>
         <p>Find useful articles and links to valuable resources. Experiment with various methods and styles. Discover cool tricks and techniques. Get used to searching, reading and practising new things!</p>
         <!-- Additional material | Blog -->
       </div>
@@ -143,21 +152,37 @@ require 'includes/nav.php';
         </div>
       </div>
       <div class="col-md-6 story">
-          <p>If you still have unanswered questions you can also visit the <a href="./help.php">help</a> section or contact us at <span class="basic-color">info@codetrip.gr</span>! We will be happy to serve you. We would also like to receive your remarks on CodeTrip and your suggestions for further improvements. We could review together some of the following points or else!</p>
-          <ul><br>
-            <li>UI & UX</li>
-            <li>Services & Enhancement</li>
-            <li>Performance & Error Handling</li>
-            <li>Efficiency & Effectiveness</li>
-            <li>Security Issues</li>
-          </ul>
-          <p>By the way, we apologise in advance for any inconvenience and urge you to report any problems you may encounter. Help us to become better! Don't forget to sign up and enjoy the whole CodeTrip experience! Have fun and build awesome stuff!</p>
-          <p id="signature">
-            <i>Kind thanks,<br>The CodeTrip Team</i>
-          </p>
+        <p>If you still have unanswered questions you can also visit the <a href="./help.php">help</a> section or contact us at <span class="basic-color">info@codetrip.gr</span>! We will be happy to serve you. We love to receive your remarks on CodeTrip and your suggestions for further improvements.</p>
+        <p>By the way, we would like to apologise in advance for any inconvenience and urge you to report any problems you may encounter. Help us to become better! Don't forget to sign up and enjoy the whole CodeTrip experience. Have fun and build awesome stuff!</p>
+        <p id="signature">
+          Kind thanks,<br><i style="font-size: 10px;">The CodeTrip Team</i>
+        </p>
+        <p>PS: We could review together some of the following points, or else!</p>
+        <!--<div class="service-checkboxes" style="padding: 10px; 0; height: 310px;">-->
+          <div class="row service-checkbox" style="padding-left: 10px;">
+            <img src="img/check.png" alt="checkbox">
+            <p> UI & UX</p>
+          </div>
+          <div class="row service-checkbox" style="padding-left: 10px;">
+            <img src="img/check.png" alt="checkbox">
+            <p> Services & Enhancement</p>
+          </div>
+          <div class="row service-checkbox" style="padding-left: 10px;">
+            <img src="img/check.png" alt="checkbox">
+            <p> Performance & Error Handling</p>
+          </div>
+          <div class="row service-checkbox" style="padding-left: 10px;">
+            <img src="img/check.png" alt="checkbox">
+            <p> Efficiency & Effectiveness</p>
+          </div>
+          <div class="row service-checkbox" style="padding-left: 10px;">
+            <img src="img/check.png" alt="checkbox">
+            <p> Security Issues</p>
+          </div>
+        <!--</div>-->
       </div>
       <div class="col-md-6 about">
-        <img src="img/user.png" alt="A targeted user.">
+        <img src="img/company.png" alt="Four colleagues hi five">
       </div>
     </div>
   </div>
