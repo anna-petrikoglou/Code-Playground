f1a1f53bdb71851711758c1d0e46d1e2dccd2cb6
Anna Petrikoglou <36823609+anna-petrikoglou@users.noreply.github.com>
2018-10-14 22:15:25 +0300

    Add files via upload

--- includes/samplefooter.php
+++ includes/samplefooter.php
@@ -3,15 +3,15 @@
   <div class="container">
     <div class="col-md-3">
       <h4>CodeTrip</h4>
-      <a href="index.php" class="footer-info">Home</a>
-      <a href="about.php" class="footer-info">About</a>
-      <a href="help.php" class="footer-info">Help</a>
+      <a href="../index.php" class="footer-info">Home</a>
+      <a href="../about.php" class="footer-info">About</a>
+      <a href="../help.php" class="footer-info">Help</a>
     </div>
     <div class="col-md-3">
       <h4>Explore</h4>
-      <a href="blog.php" class="footer-info">Blog</a>
-      <a href="tools.php" class="footer-info">Tools</a>
-      <a href="samples.php" class="footer-info">Samples</a>
+      <a href="../blog.php" class="footer-info">Blog</a>
+      <a href="../tools.php" class="footer-info">Tools</a>
+      <a href="../samples.php" class="footer-info">Samples</a>
     </div>
     <div class="col-md-3">
       <h4>Contact</h4>
@@ -19,10 +19,10 @@
     </div>
     <div class="col-md-3 social">
       <h4>Social</h4>
-      <a href="#" class="social-icon" target="_blank"><img src="img/icons/facebook.png" alt="Facebook icon" height="20" width="20" title="Like us on facebook!"></a>
+      <a href="#" class="social-icon" target="_blank"><img src="../img/icons/facebook.png" alt="Facebook icon" height="20" width="20" title="Like us on facebook!"></a>
       <!--<a href="#" class="social-icon" target="_blank"><img src="img/icons/messenger.png" alt="Messenger icon" height="20" width="20" title="Text us on messenger!"></a>
       <a href="#" class="social-icon" target="_blank"><img src="img/icons/youtube.png" alt="YouTube icon" height="20" width="20" title="Follow us on youtube!" placeholder="Youtube"></a>-->
-      <a href="#" class="social-icon" target="_blank"><img src="img/icons/instagram.png" alt="Instagram icon" height="20" width="20" title="Follow us on instagram!" placeholder="Instagram"></a>
+      <a href="#" class="social-icon" target="_blank"><img src="../img/icons/instagram.png" alt="Instagram icon" height="20" width="20" title="Follow us on instagram!" placeholder="Instagram"></a>
     </div>
   </div>
   <div class="separator"></div>
--- includes/sampleheadandnav.php
+++ includes/sampleheadandnav.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
     <!--<script src="lib/codemirror.js"></script>
     <link rel="stylesheet" href="lib/codemirror.css">
     <script src="mode/javascript/javascript.js"></script>
@@ -36,3 +36,45 @@
     <script src="https://ajaxorg.github.io/ace-builds/src/ace.js"></script>
   </head>
   <body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="../index.php">CodeTrip</a>
+        </div>
+        <div id="myNavbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../web.php">Web</a></li>
+            <?php
+              if (isset($_SESSION['u_id'])) {
+                echo '<li><a href="../php.php">PHP</a></li>
+                      <li><a href="../sql.php">MySQL</a></li>';
+              } else {
+                echo '<li class="disabled"><a href="#">PHP</a></li>
+                      <li class="disabled"><a href="#">MySQL</a></li>';
+              }
+            ?>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <?php
+              if (isset($_SESSION['u_id'])) {
+                //Use this same isset() function to change any content on any page, when a user is logged in.
+                //echo "You are logged in!";
+                echo '<li><a href="../projects.php">Projects</a></li>
+                      <li><a href="../account.php">Account</a></li>
+                      <li><a href="system/logout.php">Logout</a></li>';
+              } else {
+                echo '<li><a href="../signup.php">Sign Up</a></li>
+                      <li><a href="../login.php">Login</a></li>';
+              }
+              ?>
+              <li><a href="../about.php">Info</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
